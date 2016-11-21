4083f2851de749f4317dd7d9e6b1d41bc38ab613
BogdanLoz <bogdanlozhkin2@gmail.com>
2016-11-21 15:06:33 +0000

    Add files via upload

---
+++ Casa_exercicios/Exercicios_java/Artigo.java
@@ -0,0 +1,34 @@
+package ExerciciosPOO;
+
+public class Artigo {
+
+	private double preco;
+	public String nome;
+	
+	
+	public Artigo() {
+
+		
+	}
+	
+	
+	
+	public String getNome(){
+		return this.nome;
+	}
+	
+	public void setNome(String n){
+		this.nome = n;
+	}
+	
+	public double getPreco(){
+		return this.preco;
+	}
+	
+	public void setPreco(double p){
+		this.preco = p;
+	}
+	
+	
+
+}
---
+++ Casa_exercicios/Exercicios_java/ArtigoMain.java
@@ -0,0 +1,45 @@
+package ExerciciosPOO;
+
+import java.util.Scanner;
+
+public class ArtigoMain {
+
+	public static void main(String args[]){
+
+		double total = 0;
+		Artigo artigo1 = new Artigo();
+		Artigo artigo2 = new Artigo();
+		Artigo artigo3 = new Artigo();
+		
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Bem Vindo a Loja");
+		System.out.println("Introduza o nome do 1º Artigo");
+		String nomeA1 = input.next();
+		System.out.println("Introduza o preco do 1º Artigo");
+		double precoA1 = input.nextInt();
+		System.out.println("Introduza o nome do 2º Artigo");
+		String nomeA2 = input.next();
+		System.out.println("Introduza o nome do 2º Artigo");
+		double precoA2 = input.nextInt();
+		System.out.println("Introduza o nome do 3º Artigo");
+		String nomeA3 = input.next();
+		System.out.println("Introduza o preco do 3º Artigo");
+		double precoA3 = input.nextInt();
+		
+		artigo1.setNome(nomeA1);
+		artigo2.setNome(nomeA2);
+		artigo3.setNome(nomeA3);
+		
+		artigo1.setPreco(precoA1);
+		artigo2.setPreco(precoA2);
+		artigo3.setPreco(precoA3);
+		
+		total = artigo1.getPreco() + artigo1.getPreco() + artigo1.getPreco();
+		
+		System.out.println("O valor total dos artigos e " + total + "€");
+		
+		
+	}
+
+}
